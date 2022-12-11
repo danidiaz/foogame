4bffb2db0bbc06910a0fcefe2a0fca503f53e78a
Daniel Díaz <diaz_carrete@yahoo.com>
2022-12-11 15:21:47 +0100

    ...

--- shell.nix
+++ shell.nix
@@ -1,17 +1,15 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     packages =
-        let haskellPackages = pkgs.haskell.packages.ghc924 ;
-         in
          [
-            (haskellPackages.ghcWithPackages (hpkgs : [
+            (pkgs.haskellPackages.ghcWithPackages (hpkgs : [
                 hpkgs.lens 
                 hpkgs.servant 
                 hpkgs.servant-server 
                 hpkgs.warp
                 hpkgs.sdl2]))
-            haskellPackages.cabal-install
-            haskellPackages.haskell-language-server
+            pkgs.cabal-install
+            pkgs.haskell-language-server
             pkgs.SDL2
          ];
 }
