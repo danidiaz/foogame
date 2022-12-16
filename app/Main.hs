895e79c4c1f47b6ab4c2870b8b0223c6296ced05
Daniel Díaz <diaz_carrete@yahoo.com>
2022-12-16 23:16:49 +0100

    Draw rectangle.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NamedFieldPuns #-}
 module Main where
 
 import SDL
 import Control.Monad (unless)
+import Foreign.C
 
 main :: IO ()
 main = do
   initializeAll
-  window <- createWindow "My SDL Application" defaultWindow
+  let windowInitialSize = V2 1000 800
+  let center = (`div` 2) <$> windowInitialSize
+  let figure = P center `Rectangle` V2 100 100
+  window <- createWindow "My SDL Application" defaultWindow { windowInitialSize }
   let rendererConfig = defaultRenderer
   renderer <- createRenderer window (-1) rendererConfig
-  appLoop renderer
+  appLoop renderer figure
   destroyWindow window
   quit
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
+appLoop :: Renderer -> Rectangle CInt -> IO ()
+appLoop renderer figure = do
   events <- pollEvents
   let eventIsQPress event =
         case eventPayload event of
@@ -26,6 +31,8 @@ appLoop renderer = do
       qPressed = any eventIsQPress events
   rendererDrawColor renderer $= V4 0 0 255 255
   clear renderer
+  rendererDrawColor renderer $= V4 0 255 0 255
+  drawRect renderer $ Just figure
   -- https://stackoverflow.com/questions/63759688/sdl-renderpresent-implementation
   present renderer
-  unless qPressed (appLoop renderer)
+  unless qPressed (appLoop renderer figure)
